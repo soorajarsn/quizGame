1f8f35b554aa043e47f6dc903cfd8747659adf64
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-10 22:41:11 +0530

    add sockets

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const path = require('path');
 const routes = require('./backend/routes/mainRoutes');
 const app = express();
 
+const socket = require('socket.io');
+const http = require('http').createServer(app);
+const io = socket(http);
+
+io.on('connection',socket => {
+    let interval;
+    console.log("client connected");
+    socket.on('start_timer',token => {
+        console.log(token);
+        interval = setInterval(()=> {
+            socket.emit('time_up',token);
+        },1000*60*30);
+    })
+});
+
 app.use(express.static(path.join(__dirname,"client/dist")));
 app.use(compression());
 app.use(cors());
@@ -16,4 +31,4 @@ app.use('/',routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{console.log(`Server started running on the Port ${PORT}`)});
+http.listen(PORT,()=>{console.log(`Server started running on the Port ${PORT}`)});
