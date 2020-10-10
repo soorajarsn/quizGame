fd3f134b5902c433b0774b52336802cf8eb85a94
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-10 18:19:15 +0530

    test form

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,16 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
-
-const SubTitle = styled.p`
-color:rgb(216, 38, 62);
-font-size:1.2rem;
-@media (min-width:700px){
-    font-size:1.3rem;
-}
-@media (min-width:1000px){
-    font-size:1.4rem;
-}
+export const Title = styled.h1`
+    color:#222;
+    letter-spacing:.1rem;
+    font-size:1.5rem;
+    font-weigth:800;
+    @media (min-width:700px){
+        font-size:1.8rem;
+    }
+    @media (min-width:1000px){
+        font-size:2.5rem;
+    }
 `
-export default function(props){
-    return <SubTitle>{props.children}</SubTitle>
-}
