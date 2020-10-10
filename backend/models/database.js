ec731008a156a14b59d29eea652450d2e27c7f5d
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-10 15:38:59 +0530

    integrate database

---
+++ backend/models/database.js
@@ -0,0 +1,52 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const url = require('config').get('databaseUrl');
+const dbName = "quizme";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+client.connect();
+const getNamespace = async function (collection) {
+  try {
+    return client.db(dbName).collection(collection);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const insertOne = function (namespace, doc) {
+  namespace.insertOne(doc, function (err, r) {
+    assert.equal(null, err);
+    assert.equal(1, r.insertedCount);
+  });
+};
+const updateOne = function (namespace, query, dc) {
+  namespace.updateOne(
+    query,
+    {
+      $set: dc,
+    },
+    function (err, r) {
+      assert.equal(null, err);
+    }
+  );
+};
+const findOne = async function (namespace, query) {
+  let data = await namespace.findOne(query);
+  return data;
+};
+const findMany = function (namespace, query) {
+  let data = namespace.find(query).toArray();
+  return data;
+};
+const deleteDoc = function (namespace, query) {
+  namespace.deleteMany(query, function (err, r) {
+    assert.equal(null, err);
+    console.log("deleted successfully");
+  });
+};
+module.exports = {
+  getNamespace,
+  insertOne,
+  updateOne,
+  deleteDoc,
+  findOne,
+  findMany,
+};
