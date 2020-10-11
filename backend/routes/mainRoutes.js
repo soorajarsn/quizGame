31293612bae9bbac08db14192ae73a321f6dac28
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-11 09:57:45 +0530

    define routes

--- backend/routes/mainRoutes.js
+++ backend/routes/mainRoutes.js
@@ -1,5 +1,13 @@
+const getControllers = require('../controllers/getControllers');
+const postControllers = require('../controllers/postControllers');
 const router = require('express').Router();
 const path = require("path");
+//get controllers;
+router.route('/api/loadUser').get(getControllers.user);
+//post controllers;
+router.route('/api/post/signup').post(postControllers.signup);
+router.route('/api/post/login').post(postControllers.login);
+router.route('/api/post/quizResponse').post(postControllers.checkQuizeResponse);
 router.route('*').get((req,res)=>{
     res.sendFile(path.join(__dirname+'../../../client/dist/index.html'));
 });
