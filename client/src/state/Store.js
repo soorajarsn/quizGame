d18d6c310ffad4293870da64a692725f0afabc6c
Sooraj Shukla <soorajshukla974@gmail.com>
2020-10-11 09:56:43 +0530

    provied error state to components

--- client/src/state/Store.js
+++ client/src/state/Store.js
@@ -1,12 +1,16 @@
-import React,{useReducer} from "react";
-import {initialAuthState, authReducer} from './auth/authReducer';
+import React, { useReducer } from "react";
+import { initialAuthState, authReducer } from "./auth/authReducer";
+import { infoReducer, initialInfoState } from "./info/infoReducer";
 
 export const AuthContext = React.createContext();
-
-function Store(props){
-    const [auth,dispatch] = useReducer(authReducer,initialAuthState);
-    return <AuthContext.Provider value={{state:auth,dispatch}}>
-        {props.children}
+export const InfoContext = React.createContext();
+function Store(props) {
+  const [auth, dispatch] = useReducer(authReducer, initialAuthState);
+  const [info, dispatchInfo] = useReducer(infoReducer, initialInfoState);
+  return (
+    <AuthContext.Provider value={{ state: auth, dispatch }}>
+      <InfoContext.Provider value={{ state: info, dispatch: dispatchInfo }}>{props.children}</InfoContext.Provider>
     </AuthContext.Provider>
+  );
 }
 export default Store;
